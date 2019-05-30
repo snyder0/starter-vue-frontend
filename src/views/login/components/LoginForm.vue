ceb584f54e4a3b1856c87e2c957d185ebe860731
Bob Reilley <rreilley@vinformatix.com>
2019-05-30 10:44:47 -0500

    dynamic async component:is for login/register to use caching

---
+++ src/views/login/components/LoginForm.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <v-card-text>
+      <v-form ref="loginForm">
+        <v-text-field
+          autofocus
+          prepend-icon="email"
+          name="email"
+          label="Email"
+          type="text" 
+        />
+        <v-text-field
+          id="password"
+              
+          prepend-icon="lock"
+          name="password"
+          label="Password"
+          type="password" 
+        />
+      </v-form>
+    </v-card-text>
+    <v-card-actions>
+      <v-spacer />
+      <v-btn
+        flat
+        color="error"
+        @click="reset"
+      >
+        clear
+      </v-btn>
+      <v-btn color="primary">
+        Login
+      </v-btn>
+    </v-card-actions>
+  </div>
+</template>
+
+<script lang='ts'>
+  export default {
+    name: 'LoginForm',
+    components: {
+      
+    },
+    data() {
+      return {
+
+      }
+    },
+    computed: {
+
+    },
+    methods: {
+      switchForms () {
+
+      },
+      reset () {
+        this.$refs.loginForm.reset()
+      },
+    }
+  }
+</script>
---
+++ src/views/login/components/RegisterForm.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <v-card-text>
+      <v-form ref="registerForm">
+        <v-text-field
+          prepend-icon="person"
+          name="firstname"
+          label="First Name"
+          type="text" 
+        />
+        <v-text-field
+          prepend-icon="person"
+          name="lastname"
+          label="Last Name"
+          type="text" 
+        />
+        <v-text-field
+          prepend-icon="email"
+          name="email"
+          label="Email"
+          type="text" 
+        />
+        <v-text-field
+          id="password"
+          prepend-icon="lock"
+          name="password"
+          label="Password"
+          type="password" 
+        />
+      </v-form>
+    </v-card-text>
+    <v-card-actions>
+      <v-spacer />
+      <v-btn
+        flat
+        color="error"
+        @click="reset"
+      >
+        clear
+      </v-btn>
+      <v-btn color="primary">
+        Register
+      </v-btn>
+    </v-card-actions>
+  </div>
+</template>
+
+<script lang='ts'>
+  export default {
+    name: 'RegisterForm',
+    components: {
+      
+    },
+    data() {
+      return {
+
+      }
+    },
+    computed: {
+
+    },
+    methods: {
+      switchForms () {
+
+      },
+      reset () {
+        this.$refs.registerForm.reset()
+      },
+    }
+  }
+</script>
