0b0127cd761802075b4f17ffe04bf8839eb69137
Snyder0 <luke.snyder@selu.edu>
2019-05-31 11:34:16 -0500

    Removed test fields from login component

--- src/views/login/loginFormConfig/loginFormFields.ts
+++ src/views/login/loginFormConfig/loginFormFields.ts
@@ -18,68 +18,7 @@ const loginFormFields = [
     type: Fields.PASSWORD,
     layout: Layout.L12,
     prependIcon: 'lock'
-  },
-  {
-      key: 'date',
-      type: Fields.DATE,
-      layout: Layout.L6,
-      color: 'primary',
-      eventColor: 'warning'
-    },
-    {
-      key: 'time',
-      type: Fields.TIME,
-      layout: Layout.L6,
-      color: 'accent',
-    },
-    {
-      key: 'swtich',
-      label: 'switch',
-      type: Fields.TOGGLE,
-      layout: Layout.L6,
-      prependIcon: 'lock',
-      color: 'primary'
-    },
-    {
-      type: Fields.RADIO,
-      items: [
-        {
-          label: 'radio1',
-          value: '10',
-          layout: Layout.L12,
-          color: 'primary',
-        },
-        {
-          label: 'radio2',
-          value: '9',
-          layout: Layout.L12,
-          color: 'primary',
-        },
-        {
-          label: 'radio3',
-          value: '5',
-          layout: Layout.L12,
-          color: 'primary',
-        }
-      ]
-    },
-    {
-      key: 'select-list',
-      label: 'Select List',
-      type: Fields.SELECTLIST,
-      multiple: true,
-      layout: Layout.L6,
-      items: [
-        {
-          text: 'Option 1',
-          value: 1
-        },
-        {
-          text: 'Option 2',
-          value: 2
-        }
-      ]
-    }
+  }
 ]
 
 export default loginFormFields
